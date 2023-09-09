72715db5364925937b7a0c58330ba0ce7ec7dfcf
unknown <ilhsm672016@gmail.com>
2023-09-09 21:13:34 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+  {path: '',component: HomeComponent},
   {path: 'home',component: HomeComponent},
   {path: 'about',component: AboutComponent},
   {path: 'portfolio',component: PortfolioComponent},
-  {path: 'contact',component: ContactComponent}
+  {path: 'contact',component: ContactComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
